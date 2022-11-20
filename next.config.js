830a48889a9c41b11cde1c50e31cd295a429bb98
joseph emmanuel kayode (iemarjay) <ejoseph@wikimedia.org>
2022-11-20 23:12:17 +0400

    [WIP] swap interface

--- next.config.js
+++ next.config.js
@@ -2,6 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  future: { webpack5: true },
+  webpack: config => {
+    // Unset client-side javascript that only works server-side
+    config.resolve.fallback = { fs: false, module: false }
+    return config
+  },
 }
 
-module.exports = nextConfig
+const {
+  createVanillaExtractPlugin
+} = require('@vanilla-extract/next-plugin');
+const withVanillaExtract = createVanillaExtractPlugin();
+
+module.exports = withVanillaExtract(nextConfig)
